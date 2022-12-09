0e3d2a97a5580db71ab896d50a6efeb67b310283
asanme <u.alexsanme@gmail.com>
2022-12-09 12:36:43 +0100

    Added current neovim configuration

---
+++ nvim/lua/user/colorscheme.lua
@@ -0,0 +1,8 @@
+-- Load colorscheme
+local colorscheme = "tokyonight-night"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
