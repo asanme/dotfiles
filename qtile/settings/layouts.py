da3c58ca6fa69bdfb00dd910b256022ead31f24f
asanme <u.alexsanme@gmail.com>
2023-02-10 16:12:33 +0100

    Refactoring Qtile config files

---
+++ qtile/settings/layouts.py
@@ -0,0 +1,30 @@
+from libqtile import 
+from libqtile import layout
+from libqtile.config import Match
+
+layouts = [
+    layout.Columns(
+        margin=6, 
+        border_focus='#ffffff',
+	    border_normal='#4ea8de', 
+        border_width=3
+    ),
+
+    layout.Max(),
+]
+
+floating_layout = layout.Floating(
+	border_focus='#1F1D2E',
+	border_normal='#1F1D2E',
+	border_width=0,
+    float_rules=[
+        # Run the utility of `xprop` to see the wm class and name of an X client.
+        *layout.Floating.default_float_rules,
+        Match(wm_class="confirmreset"),  # gitk
+        Match(wm_class="makebranch"),  # gitk
+        Match(wm_class="maketag"),  # gitk
+        Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(title="branchdialog"),  # gitk
+        Match(title="pinentry"),  # GPG key password entry
+    ]
+)
