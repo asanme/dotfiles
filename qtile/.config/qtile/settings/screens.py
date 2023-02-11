363366ae7c3bf260bcc5eb295e41328a146fea28
asanme <u.alexsanme@gmail.com>
2023-02-11 18:02:25 +0100

    Finished Qtile config

--- qtile/.config/qtile/settings/screens.py
+++ qtile/.config/qtile/settings/screens.py
@@ -1,154 +1,36 @@
-from libqtile import bar, widget
+import subprocess
+
+from libqtile import bar
 from libqtile.config import Screen
+from libqtile.log_utils import logger
 
-screens = [
-    Screen(
-        wallpaper='~/wallpapers/catalina-wallpaper.jpg',
-        wallpaper_mode='fill',
-        top=bar.Bar(
-            [
-				widget.Spacer(
-                    length=30,
-                    background='#1F1D2E',
-                ),
+from .widgets import primary_widgets, secondary_widgets
 
 
-                # First Separator
-                widget.Image(
-                    filename='~/.config/qtile/Assets/launch_Icon.png',
-                    margin=2,
-                    background='#1F1D2E',
-                ),
+def status_bar(widgets):
+    return bar.Bar(widgets, 40, opacity=1)
 
-                widget.Image(
-                    filename='~/.config/qtile/Assets/6.png',
-                ),
+screens = [Screen(wallpaper='~/wallpapers/catalina-wallpaper.jpg', wallpaper_mode='fill', top=status_bar(primary_widgets()))]
 
+xrandr = "xrandr | grep -w 'connected'"
 
-                # Second Separator
-                widget.GroupBox(
-                    fontsize=25,
-                    borderwidth=3,
-                    highlight_method='block',
-                    active='#7F61A7',
-                    block_highlight_text_color="#CFB3E5",
-                    highlight_color='#4B427E',
-                    inactive='#BD85CB',
-                    foreground='#4B427E',
-                    background='#4B427E',
-                    this_current_screen_border='#52548D',
-                    this_screen_border='#52548D',
-                    other_current_screen_border='#52548D',
-                    other_screen_border='#52548D',
-                    urgent_border='#52548D',
-                    rounded=True,
-                    disable_drag=True,
-                 ),
+command = subprocess.run(
+    xrandr,
+    shell=True,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+)
 
-                widget.Image(
-                    filename='~/.config/qtile/Assets/5.png',
-                ),
+if command.returncode != 0:
+    error = command.stderr.decode("UTF-8")
+    logger.error(f"Failed counting monitors using {xrandr}:\n{error}")
+    connected_monitors = 1
+else:
+    # Check the output of the xrandr command, if a monitor is connected
+    # but turned off, then it will not show any resolution for that montior.
+    xrandr_output = command.stdout.decode("UTF-8").split("\n")[:-1]
+    resolutions = map(lambda output: output.split(" ")[2], xrandr_output) 
+    connected_monitors = len([r for r in resolutions if not r.startswith("(")])
 
-                widget.CurrentLayoutIcon(
-                    background='#52548D',
-                    padding = 0,
-                    scale = 0.5,
-                ),
-
-                widget.CurrentLayout(
-                    background='#52548D',
-                    font= 'JetBrains Mono Bold',
-                ),
-
-                widget.Image(
-                    filename='~/.config/qtile/Assets/4.png',                
-                ),
-
-                widget.WindowName(
-                    background = '#7676B2',
-                    format = "{name}",
-                    font='JetBrains Mono Regular',
-                    empty_group_string = 'Desktop',
-                    center_aligned= True
-                ),
-
-                #Third Separator
-                widget.Image(
-                    filename='~/.config/qtile/Assets/3.png',                
-                ),   
-
-                widget.Systray(
-                    background='#52548D',
-                    fontsize=2,
-                ),
-
-                widget.TextBox(
-                    text=' ',
-                    background='#52548D',
-                ),
-
-
-                widget.Image(
-                    filename='~/.config/qtile/Assets/2.png',                
-                    background='#52548D',
-                ),                       
-                                                
-                widget.TextBox(
-                    text='',
-                    size=20,
-                    font='JetBrains Mono Bold',
-                    background='#4B427E',
-                ),
-                                
-                widget.Battery(
-                    format=' {percent:1.0%}',
-                    font="JetBrains Mono ExtraBold",
-                    fontsize=25,
-                    padding=10,
-                    background='#4B427E',
-                ),                     
-                 
-               
-                widget.Memory(format='﬙{MemUsed: .0f}{mm}',
-                    font="JetBrains Mono Bold",
-                    fontsize=25,
-                    padding=10,
-                    background='#4B427E',
-                ),
-
-                widget.TextBox(
-                    text="",
-                    font="Font Awesome 6 Free Solid",
-                    fontsize=25,
-                    padding=0,
-                    background='#4B427E',
-                ),
-                
-                widget.PulseVolume(
-                    font='JetBrains Mono Bold',
-                    fontsize=25,
-                    padding=10,
-                    background='#4B427E',
-                ),                
-
-                widget.Image(
-                    filename='~/.config/qtile/Assets/1.png',                
-                    background='#4B427E',
-                ),
-        
-                widget.Clock(
-                    format='  %R',
-                    background='#1F1D2E',
-                    font="JetBrains Mono Bold",
-                ),
-
-                widget.Spacer(
-                    length=18,
-                    background='#1F1D2E',
-                ),
-            ],
-            40,
-            margin = [6,6,6,6]
-        ),
-    ),
-]
+for _ in range(1, connected_monitors):
+    screens.append(Screen(wallpaper='~/wallpapers/mojave-wallpaper.jpg', wallpaper_mode='fill',top=status_bar(secondary_widgets())))
--- qtile/.config/qtile/settings/widgets.py
+++ qtile/.config/qtile/settings/widgets.py
@@ -12,7 +12,7 @@ def separator():
     return widget.Sep(**base(), linewidth=0, padding=5)
 
 
-def icon(fg='text', bg='dark', fontsize=16, text="?"):
+def icon(fg='text', bg='dark', fontsize=24, text="?"):
     return widget.TextBox(
         **base(fg, bg),
         fontsize=fontsize,
@@ -21,7 +21,7 @@ def icon(fg='text', bg='dark', fontsize=16, text="?"):
     )
 
 
-def powerline(fg="light", bg="dark", fontsize=37, padding=-3):
+def powerline(fg="light", bg="dark", fontsize=56, padding=-7):
     return widget.TextBox(
         **base(fg, bg),
         text="", # Icon: nf-oct-triangle_left
@@ -30,12 +30,12 @@ def powerline(fg="light", bg="dark", fontsize=37, padding=-3):
     )
 
 
-def workspaces(icon_fontsize=19, window_name_font_size=14): 
+def workspaces(icon_fontsize=27, window_name_font_size=24): 
     return [
         separator(),
         widget.GroupBox(
             **base(fg='light'),
-            font='FiraCode NFM',
+            font='UbuntuMono Nerd Font',
             fontsize=icon_fontsize,
             margin_y=3,
             margin_x=0,
@@ -66,37 +66,21 @@ def primary_widgets():
 
         separator(),
 
-        powerline('color4', 'dark'),
+        powerline('color1', 'dark'),
 
-        icon(bg="color4", text=' '), # Icon: nf-fa-download
+        widget.Memory(**base(bg='color1'), format=' {MemUsed: .0f}{mm} ', font="JetBrains Mono Bold", fontsize=25, padding=10),
 
-        widget.CheckUpdates(
-            background=colors['color4'],
-            colour_have_updates=colors['text'],
-            colour_no_updates=colors['text'],
-            no_update_string='0',
-            display_format='{updates}',
-            update_interval=1800,
-            custom_command='checkupdates',
-        ),
-
-        powerline('color3', 'color4'),
-
-        icon(bg="color3", text=' '),  # Icon: nf-fa-feed
-
-        widget.Net(**base(bg='color3'), interface='wlo1'),
-
-        powerline('color2', 'color3'),
+        powerline('color2', 'color1'),
 
         widget.CurrentLayoutIcon(**base(bg='color2'), scale=0.65),
 
-        widget.CurrentLayout(**base(bg='color2')),
+        widget.CurrentLayout(**base(bg='color2'), fmt='{}  ', font='JetBrains Mono Bold'),
 
         powerline('color1', 'color2'),
 
         icon(bg="color1", text=' '), # Icon: nf-mdi-calendar_clock
 
-        widget.Clock(**base(bg='color1'), format='%d/%m/%Y - %H:%M '),
+        widget.Clock(**base(bg='color1'), format='%d/%m/%Y -   %H:%M '),
 
         powerline('dark', 'color1'),
 
@@ -114,20 +98,21 @@ def secondary_widgets():
 
         widget.CurrentLayoutIcon(**base(bg='color1'), scale=0.65),
 
-        widget.CurrentLayout(**base(bg='color1'), padding=5),
+        widget.CurrentLayout(**base(bg='color1'), fmt='{}  ', padding=5, font='JetBrains Mono Bold'),
 
         powerline('color2', 'color1'),
 
         icon(bg="color2", text=' '), # Icon: nf-mdi-calendar_clock
 
-        widget.Clock(**base(bg='color2'), format='%d/%m/%Y - %H:%M '),
+        widget.Clock(**base(bg='color2'), format='%d/%m/%Y -   %H:%M '),
 
         powerline('dark', 'color2'),
     ]
 
 widget_defaults = {
     'font': 'FiraCode NFM',
-    'fontsize': 14,
+    'fontsize': 24,
     'padding': 1,
 }
+
 extension_defaults = widget_defaults.copy()
